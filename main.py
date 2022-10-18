35ff3f39c956fc43b69b47ff7f47677b9128e8b0
justanotherinternetguy <hendryxu1@gmail.com>
2022-10-18 14:04:02 -0400

    e

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from webdriver_manager.chrome import ChromeDriverManager
 
-exec(open("./init.py").read())
+# exec(open("./init.py").read())
 
 load_dotenv()
 USERNAME = os.getenv("usr")
@@ -20,6 +20,18 @@ def login(url, usernameID, username, passwordID,  password, submitID):
     driver.find_element(By.NAME, passwordID).send_keys(password)
     driver.find_element(By.ID, submitID).click()
 
+def scrape(url):
+    page = requests.get(url)
+    soup = BeautifulSoup(page.content, "html.parser")
 
-login("https://millburn.powerschool.com/public/home.html", "account", USERNAME, "pw", PASSWORD, "btn-enter-sign-in")
+
+    classes = soup.find_all("a")
+    print(soup)
+    print(url)
+    
+
+
+if __name__ == "__main__":
+    login("https://millburn.powerschool.com/guardian/home.html", "account", USERNAME, "pw", PASSWORD, "btn-enter-sign-in")
+    scrape("https://millburn.powerschool.com/guardian/home.html")
 
